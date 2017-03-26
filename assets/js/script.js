eaa1b44bcbb5f29617125bfd040d4cb0dc448f35
sendz <sendzation@gmail.com>
2017-03-26 17:07:29 +0700

    Use hash

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var URL = "https://invitation-sendz.rhcloud.com/"
 
 $(document).ready(function () {
-    var token = location.pathname.split("#")[1];
+    var token = location.hash.split("#")[1];
     console.log(location);
     console.log(token);
     getTokenDetails(token);
