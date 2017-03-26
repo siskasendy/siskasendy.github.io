a402cda85d48a34a872523df16a0d79817453e85
sendz <sendzation@gmail.com>
2017-03-26 18:41:57 +0700

    Add logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     var token = location.hash.split("#")[1];
     getTokenDetails(token);
     loadingScreen = pleaseWait({
+        logo: 'assets/img/logo.png',
         backgroundColor: '#f06060',
         loadingHtml: "<div class='sk-spinner sk-spinner-wave'><div class='sk-rect1'></div><div class='sk-rect2'></div><div class='sk-rect3'></div><div class='sk-rect4'></div><div class='sk-rect5'></div></div>"
     });
