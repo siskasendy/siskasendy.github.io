240c2254dfb5e9759dddf4b7add62b860759832f
sendz <sendzation@gmail.com>
2017-03-26 17:22:47 +0700

    Use script.js only

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,28 @@ $(document).ready(function () {
 function getTokenDetails(token) {
     $.get(URL + "auth/" + token, function (data) {
         $('#token_name').html(data.name);
-        langCode = data.language;
+        $('#token_location').html(data.address);
+        translatePage(data.address);
         console.log(data);
     });
+}
+
+function translatePage(langCode) {
+    var langs = ['EN', 'ID'];
+    var langJS = null;
+
+
+    var translate = function (jsdata) {
+        $("[tkey]").each(function (index) {
+            var strTr = jsdata[$(this).attr('tkey')];
+            $(this).html(strTr);
+        });
+    }
+
+    console.log(langCode);
+
+    if (langCode in langs)
+        $.getJSON('assets/js/language/' + langCode + '.json', translate);
+    else
+        $.getJSON('assets/js/language/EN.json', translate);
 }
