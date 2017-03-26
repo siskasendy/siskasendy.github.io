b8e2ff6ba70727a296b9c4f48abaec1721d23228
sendz <sendzation@gmail.com>
2017-03-26 16:32:58 +0700

    Call method on ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var URL = "http://invitation-sendz.rhcloud.com/"
 
 $(document).ready(function () {
     var token = location.pathname.split("/")[2];
-
+    getTokenDetails(token);
 });
 
 function getTokenDetails(token) {
