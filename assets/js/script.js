100368f10d04b6ad6149a90a6548f1f411e4909c
sendz <sendzation@gmail.com>
2017-03-26 17:14:33 +0700

    Use name call

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,12 @@ var URL = "https://invitation-sendz.rhcloud.com/"
 
 $(document).ready(function () {
     var token = location.hash.split("#")[1];
-    console.log(location);
-    console.log(token);
     getTokenDetails(token);
 });
 
 function getTokenDetails(token) {
     $.get(URL + "auth/" + token, function (data) {
+        $('#token_name').html(data.name);
         console.log(data);
     });
 }
